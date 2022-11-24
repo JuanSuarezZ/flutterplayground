930f61993089274a28cfb4f4857cee1bd167b96e
juan suarez <juansebastiansuarez28@gmail.com>
2022-11-24 00:16:26 -0500

    set up en mac with drawer 3d done

---
+++ android/app/src/main/kotlin/com/example/flutter_playground/flutter_playground/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_playground.flutter_playground
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+// import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'core/routes.dart';
+
+Future<void> main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: "3dImagesExample",
+      routes: getAplicationRoutes,
+    );
+  }
+}
