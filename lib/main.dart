bc468d52e3661d0bdb418413a41667b700a0e828
juan suarez <jsuarez@singularagency.co>
2022-11-25 22:57:12 -0500

    firebase android settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
-// import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'core/routes.dart';
 
 Future<void> main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const App());
 }
 
